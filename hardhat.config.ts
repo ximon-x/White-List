7e1b415a62e2aeb70e744416e1202897cc7614f1
Simon Samuel <salviensky@gmail.com>
2022-10-20 21:14:32 +0100

    Deployed contract to goerli

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const { ALCHEMY_API_URL, METAMASK_PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: ALCHEMY_API_URL,
+      accounts: [METAMASK_PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
