b0165f0908b8b3d958eac963fdc9973f70529e3a
Simon Samuel <salviensky@gmail.com>
2022-10-20 23:15:27 +0100

    Dapp Completed

---
+++ Constants.ts
@@ -0,0 +1,5 @@
+import whitelist from "./artifacts/contracts/Whitelist.sol/Whitelist.json";
+
+export const WHITELIST_CONTRACT_ADDRESS =
+  "0x9bc835359D63Ad432C9F1794C28c08228FEb459A";
+export const WHITELIST_CONTRACT_ABI = whitelist.abi;
